f9eb11680982272dee88437745111b60b915da00
Stephen Sugden <me@stephensugden.com>
2019-08-05 10:33:31 +0200

    Convert Interpreter into a real class

--- lib/xstate/interpreter.rb
+++ lib/xstate/interpreter.rb
@@ -1,12 +1,20 @@
 module XState
-  Interpreter = Struct.new(:machine, :state, :side_effects) do
+  class Interpreter
+    def initialize(machine, state, side_effects)
+      @machine = machine
+      @state = state
+      @side_effects = side_effects
+    end
+
+    attr_reader :state
+
     def send_event(event)
-      next_state = machine.transition(state, event)
+      next_state = @machine.transition(@state, event)
       next_state.actions.each do |action|
-        side_effects.dispatch_action(action, @state.context, event)
+        @side_effects.dispatch_action(action, @state.context, event)
       end
-      self.state = next_state
-      self.state.value
+      @state = next_state
+      @state.value
     end
   end
 end
